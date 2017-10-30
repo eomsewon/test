982353a112af81719f1767ea09d7a3242d5a41bb
student <student@M11015>
2017-10-30 17:19:00 +0900

    두 번째 작업 추가
    추가되었습니다

--- gitTest/src/gitTest/Test.java
+++ gitTest/src/gitTest/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git");
+		System.out.println("추가작업을 하였습니다.");
 	}
 
 }
